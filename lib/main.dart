91bb4b9fc714ee6d4170d0d52ef2075464a5d175
muzammil <mujmildafedaar@gmail.com>
2021-10-09 23:23:11 +0530

    core file generated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'dart:io';
 
+// core
+//  --data_model
+//  	--api
+//  --enums
+//  --model
+//  --services
+//  	--api
+//  --viewmodels
+// ui
+//  --shared
+//  --views
+// utils
+//  --constant
+
 startBuildingFiles() {
-  new File('lib/test/test.dart').create(recursive: true).then((File file) {
-    print("Test file has successfully generated...");
-    // Stuff to do after file has been created...
-  });
+  try {
+    new File('core/data_model/sample.dart')
+        .create(recursive: true)
+        .then((File file) {
+      file.writeAsString(
+          "class Login {  String? name; String? email; String? pass; Login({this.name, this.email, this.pass}); factory Login.fromJson(Map<String, dynamic> json) { var payload = {};  return Login( name: payload['name'], email: payload['payload'],  pass: payload['pass'],);  }}");
+      print("Test file has successfully generated...");
+      // Stuff to do after file has been created...
+    });
+  } catch (e) {
+    print("Error: $e");
+  }
 }
