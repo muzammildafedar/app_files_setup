86591d548a4629ddc4bba2ebc0879f2b73728ea1
muzammil <mujmildafedaar@gmail.com>
2021-10-12 18:47:28 +0530

    condition changed

--- bin/start.dart
+++ bin/start.dart
@@ -6,7 +6,7 @@ import 'package:app_files_setup/main.dart' as start;
 
 void main(List<String> argument) {
   stdout.write(packageIntro('1.0.1'));
-  if (argument == null) {
+  if (argument != null) {
     start.startBuildingFiles(argument);
   } else {
     start.startBuildingFiles(fileNames);
